ddaa1383d6e7b5cf077d25980a8ba0c6aa8ca281
srajan <kumarsrajan02@gmail.com>
2023-10-17 17:14:08 +0530

    next-app, shadcn init, theme-toggle

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ThemeToggle from "@/components/ThemeToggle";
+
+export default function Home() {
+  return (
+    <main>
+      <ThemeToggle />
+    </main>
+  );
+}
