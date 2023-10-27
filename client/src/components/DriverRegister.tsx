4124dfc1f44b3586ddb9e963ec7de7fbce48f479
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-10-27 19:29:26 +0530

    Driver registration  Added

---
+++ client/src/components/DriverRegister.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+// import { Icons } from "@/components/ui/icons";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { SyntheticEvent, useState } from "react";
+
+import axios from "axios";
+
+export default function DriverRegister() {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("http://localhost:3001/Driver/register", {
+        username,
+        phone,
+        email,
+        password,
+      });
+      alert("Registration Completed! Login to continue");
+      window.location.href = "/driver/login";
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Card>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Register</CardTitle>
+        </CardHeader>
+        <CardContent className="grid w-96 gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="username">Name</Label>
+            <Input
+              id="username"
+              type="text"
+              placeholder="Full Name"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="phone">Phone</Label>
+            <Input
+              id="phone"
+              type="number"
+              placeholder="Your ten digit mobile number"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Strong Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="grid gap-3 w-full">
+            <Button className="w-full" type="submit">
+              Create account
+            </Button>
+            <div className="space-x-2 text-sm">
+              <span>Already have an account?</span>
+              <Link href="/login" className="hover:underline">
+                Login
+              </Link>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
