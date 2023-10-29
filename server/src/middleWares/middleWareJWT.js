77e960c1304e9d611ea756b508c1db51d99046de
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-10-29 16:27:22 +0530

    MiddleWare Modified

--- server/src/middleWares/middleWareJWT.js
+++ server/src/middleWares/middleWareJWT.js
@@ -1,6 +1,9 @@
+// Middleware to verify JWT token in request headers
 export const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
+
   if (token) {
+    // Verify the token using the "secret" key
     jwt.verify(token, "secret", (err) => {
       if (err) return res.sendStatus(403); // Token verification failed
       next(); // Proceed to the next middleware or route
